adbdaf6e9239ca86b810a782c2946393bb1fd3f5
BayhanR <91837307+BayhanR@users.noreply.github.com>
2024-04-27 01:06:57 +0300

    Add files via upload

---
+++ CaptureText2/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace CaptureText2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(535, 240);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(192, 240);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(271, 20);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(900, 534);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ CaptureText2/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Tesseract;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace CaptureText2
+{
+    public partial class Form1 : Form
+    {
+        private bool capturing = false;
+        private Point startPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            HookGlobalEvents();
+        }
+
+        private void HookGlobalEvents()
+        {
+            MouseHook.Start();
+            KeyboardHook.Start();
+            MouseHook.MouseAction += MouseHook_MouseAction;
+            KeyboardHook.KeyAction += KeyboardHook_KeyAction;
+        }
+
+        private void MouseHook_MouseAction(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (!capturing)
+                {
+                    startPoint = e.Location;
+                    capturing = true;
+                    Cursor = Cursors.Cross;
+                }
+                else
+                {
+                    capturing = false;
+                    Cursor = Cursors.Default;
+
+                    Rectangle selection = new Rectangle(
+                        Math.Min(startPoint.X, e.X),
+                        Math.Min(startPoint.Y, e.Y),
+                        Math.Abs(startPoint.X - e.X),
+                        Math.Abs(startPoint.Y - e.Y)
+                    );
+
+                    Bitmap screenshot = CaptureScreen(selection);
+                    string recognizedText = RecognizeText(screenshot);
+                    Clipboard.SetText(recognizedText);
+                    MessageBox.Show("Seçilen alanın metni panoya kopyalandı.");
+
+                    MouseHook.Stop();
+                    KeyboardHook.Stop();
+                }
+            }
+        }
+
+        private void KeyboardHook_KeyAction(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape && capturing)
+            {
+                capturing = false;
+                Cursor = Cursors.Default;
+                MouseHook.Stop();
+                KeyboardHook.Stop();
+            }
+        }
+
+        private Bitmap CaptureScreen(Rectangle area)
+        {
+            Bitmap screenshot = new Bitmap(area.Width, area.Height);
+            using (Graphics g = Graphics.FromImage(screenshot))
+            {
+                g.CopyFromScreen(area.Location, Point.Empty, area.Size);
+            }
+            return screenshot;
+        }
+
+        private string RecognizeText(Bitmap image)
+        {
+            using (var engine = new TesseractEngine("./tessdata", "eng", EngineMode.Default))
+            {
+                using (var page = engine.Process(image, PageSegMode.Auto))
+                {
+                    return page.GetText();
+                }
+            }
+        }
+    }
+
+    public static class MouseHook
+    {
+        public static event MouseEventHandler MouseAction = delegate { };
+
+        public static void Start()
+        {
+            _hookID = SetHook(_proc);
+        }
+
+        public static void Stop()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static LowLevelMouseProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr SetHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private const int WH_MOUSE_LL = 14;
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && MouseMessages.WM_LBUTTONDOWN == (MouseMessages)wParam)
+            {
+                MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                MouseAction(null, new MouseEventArgs(MouseButtons.Left, 0, hookStruct.pt.x, hookStruct.pt.y, 0));
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private enum MouseMessages
+        {
+            WM_LBUTTONDOWN = 0x0201
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MSLLHOOKSTRUCT
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+
+    public static class KeyboardHook
+    {
+        public static event KeyEventHandler KeyAction = delegate { };
+
+        public static void Start()
+        {
+            _hookID = SetHook(_proc);
+        }
+
+        public static void Stop()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                KeyAction(null, new KeyEventArgs((Keys)vkCode));
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
